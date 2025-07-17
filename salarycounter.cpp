84d6301fef0bf4a7a24ff821ca8465363b2e59be
Aaditya Bhadane <asbdev07@gmail.com>
2025-07-17 21:54:19 +0530

    Add files via upload

---
+++ salarycounter.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{	
+ 	int id,half_day,full_day;
+	float salary,bonus,fund,total_amount,total_cutoff;
+	
+	cout<<"Kindly Enter the Id=";
+	cin>>id;
+	cout<<"Kindly Enter the Salary=";
+	cin>>salary;
+	cout<<"Kindly Enter the Bonus=";
+	cin>>bonus;
+	cout<<"Kindly Enter the full day off=";
+	cin>>full_day;
+	cout<<"Kindly Enter half day off=";
+	cin>>half_day;
+	cout<<"Kindly Enter Fund amount=";
+	cin>>fund;
+	
+	salary=salary+bonus;
+	total_cutoff=(1500*full_day)+(750*half_day)+fund;
+	
+	total_amount=salary-total_cutoff;
+	
+	if(total_amount<=0)
+	{
+		cout<<"The salary is loss of ="<<total_amount<<endl;
+	}
+	else
+	{
+	    cout<<"The salary credits amount ="<<total_amount<<endl;	
+	}
+	cout<<"Data Stored Successfully";
+}
